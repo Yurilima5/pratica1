58a34142e1bb26af769760f091a3e9c2ffb971d5
yurilima5 <yurilimarosa12@gmail.com>
2023-08-15 09:39:56 -0300

    second-commit

---
+++ script.js
@@ -0,0 +1,40 @@
+//comentário
+
+let cor = "tomato"
+
+let doce = "chiclete"
+
+console.log("a minha cor favorita é " + cor)
+
+//template string
+console.log(`meu doce favorito é ${doce}  `)
+
+//condicional simples
+
+let idadeMaiorQueDezoito = true
+
+if(idadeMaiorQueDezoito === true){
+console.log("você é maior de dezoito")
+}else{
+    console.log("você não tem dezoito anos")
+}
+
+// Operadores de condição 
+// operadores de comparação
+// > -> maior que
+// < -> menor que
+// >= -> maior ou igual a
+// <= -> menor ou igual a 
+// == -> comparador de valor
+// === -> estritamente igual compara valor e tipo
+// != -> diferente de, verifica se os valores são diferentes
+// !== estritamente desigual, verifica se o valor e tipo são diferentes]
+
+let numeroDaSorte = 3
+
+if(numeroDaSorte === 3){
+    console.log("vou ganhar na megasena !!!")
+
+}else{
+    console.log ("coitado de mim deixa p proxima")
+}
